aa969a2f8c5ea590675fdac2d74fdfb6537d1570
428xx <albertx361@googlemail.com>
2020-01-12 15:06:58 -0800

    t

--- EPI/7.5_TestOverlapWithCycles.py
+++ EPI/7.5_TestOverlapWithCycles.py
@@ -2,8 +2,13 @@ from LinkedList import Node
 
 #test overlap for lists that potentially have cycles
 #1 and 2 step to check if they are ever on same node
+#cases:
+#  neither list has cycle
+#  one list has cycle
+#  both lists have cycles
 def testOverlap(L1, L2):
-  pass
+  L1.next.next
+  L2.next
 
 def main():
   pass
