e435ee318e2fca44f571ce9c9fcd7e3358f041ea
428xx <albertx361@googlemail.com>
2020-01-19 20:51:22 -0800

    t

---
+++ EPI/9.10_ComputeSuccessor.py
@@ -0,0 +1,13 @@
+from TreeNode import NodeWithParent as Node
+import TreeNode
+
+#if node has right subtree it must be far left node of right subtree
+#else, far left node of first right ancestor
+def findSuccessor(node):
+  pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
