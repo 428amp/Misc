e611d40c1adf76fb93c10d48d9b8a69ac33e5d10
428xx <albertx361@googlemail.com>
2020-01-26 10:44:42 -0800

    10.5

--- EPI/10.5_ComputeMedianStream.py
+++ EPI/10.5_ComputeMedianStream.py
@@ -1,26 +1,40 @@
 import heapq
 
+# def median(stream):
+#   leftHeap = [] #maxheap left
+#   rightHeap = [] #minheap right
+#   first = True
+#   for num in stream:
+#     if first:
+#       heapq.heappush(leftHeap, num)
+#       first = False
+#       continue
+#     if num >= -leftHeap[0]:
+#       heapq.heappush(rightHeap, num)
+#     else:
+#       heapq.heappush(leftHeap, -num)
+#     while len(leftHeap) > len(rightHeap):
+#       heapq.heappush(rightHeap, -heapq.heappop(leftHeap))  
+#     while len(leftHeap) < len(rightHeap):
+#       heapq.heappush(leftHeap, -heapq.heappop(rightHeap))
+#   if len(stream)%2 != 0:
+#     return -heapq.heappop(leftHeap)
+#   else:
+#     return (-heapq.heappop(leftHeap)+heapq.heappop(rightHeap))/2
+
 def median(stream):
   leftHeap = [] #maxheap left
   rightHeap = [] #minheap right
-  first = True
   for num in stream:
-    if first:
-      heapq.heappush(leftHeap, num)
-      first = False
-      continue
-
-    if num > -leftHeap[0]:
-      heapq.heappush(rightHeap, num)
-    else:
-      heapq.heappush(leftHeap, -num)
-
-    while len(leftHeap) < len(rightHeap):
-      heapq.heappush(leftHeap, -heapq.heappop(rightHeap))
+    heapq.heappush(rightHeap, num)
+    n = heapq.heappop(rightHeap)
+    heapq.heappush(leftHeap, -n)
+    if len(leftHeap) > len(rightHeap):
+      heapq.heappush(rightHeap, -heapq.heappop(leftHeap))
   if len(stream)%2 != 0:
-    return -heapq.heappop(leftHeap)
+    return rightHeap[0]
   else:
-    return (-heapq.heappop(leftHeap)+heapq.heappop(rightHeap))/2
+    return (-leftHeap[0]+rightHeap[0])/2
 
 def dumb_median(stream):
   stream.sort()
@@ -32,12 +46,14 @@ def dumb_median(stream):
 def main():
   import random
   P = [i for i in  range(100)]
+
   for i in range(1000):
     stream = random.choices(P, k=random.randrange(10, 50))
     med = median(stream)
     dumb_med = dumb_median(stream)
     if med != dumb_med:
       print('mismatch')
+      print(sorted(stream))
       print(med)
       print(dumb_med)
       break
